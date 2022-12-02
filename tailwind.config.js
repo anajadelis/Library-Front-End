8dd2dd1d70917de314a5cd6affc37e227591d0e1
Ana Jade de Lis e Silva <92063319+anajadelis@users.noreply.github.com>
2022-12-02 20:05:41 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  dark:"class",
+  theme: {
+    fontFamily: {
+      primary: 'Playfair Display',
+      body: 'Work Sans',
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        lg: '3rem',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {
+      content: {
+        about: 'url("/src/assets/img/outline-text/about.svg")',
+        portfolio: 'url("/src/assets/img/outline-text/portfolio.svg")',
+        hobbies: 'url("/src/assets/img/outline-text/hobbies.svg")',
+        contact: 'url("/src/assets/img/outline-text/contact.svg")',
+      },
+      colors: {
+        primary: '#050402',
+        secondary: '#1C1D24',
+        tertiary: '#131419',
+        accent: {
+          DEFAULT: 'rgb(139 92 246)',
+          hover: '#925a2b',
+        },
+        paragraph: '#878e99',
+      },
+    },
+  },
+  plugins: [],
+};
