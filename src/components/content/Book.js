c9ff54ff75b1ae28b860ac8cbdd7a7ba78681881
HigoHenrique <99215544+HigoHenrique@users.noreply.github.com>
2022-12-08 15:07:23 -0300

    fiz ajustes para conexão dos livros do banco de dados

--- src/components/content/Book.js
+++ src/components/content/Book.js
@@ -4,13 +4,11 @@ const Book = ({ item }) => {
   return (
     <div key={item.id} className='flex flex-col items-center text-center'>
       <div className='mb-8 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 '>
-        <a href={item.href} target='_black'><img className=' h-96 object-cover md:w-50 rounded-2xl' src={item.image} alt='' /></a>
+        <a href={item.pdf} target='_black'><img className=' h-96 object-cover md:w-50 rounded-2xl' src={item.image} alt='' /></a>
       </div>
       <p className='capitalize text-violet-500 text-sm mb-3'>{item.category}</p>
-      <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
-      <p className='text-base max-w-md'>{item.description}</p>
-      <a href={item.estante}>
-      </a>
+      <h3 className='text-2xl font-semibold capitalize mb-3'>{item.title}</h3>
+      <p className='text-base max-w-md'>{item.synopsis}</p>
     </div>
   );
 };
